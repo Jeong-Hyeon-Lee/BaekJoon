835bf4022887703439587714ee1e68efaa320cc9
JeongHyun Lee <86969518+Jeong-Hyeon-Lee@users.noreply.github.com>
2022-04-04 23:47:27 +0900

    Add files via upload

---
+++ LCS_BOJ9251.cpp
@@ -0,0 +1,31 @@
+﻿/*
+﻿dp 배열에 인덱스 위치까지의 LCS를 저장하기로 하고 점화식을 만들었다.
+만약 A의 i번째 알파벳과 B의 j번째 알파벳이 같다면 그 전까지의 LCS에 1을 더해주고,
+알파벳이 다르다면 그 전까지의 LCS들을 비교해 큰 값을 해당 위치에 저장한다. 마지막 문자까지 다이나믹 프로그래밍을 완료한 후 답을 출력한다.
+*/
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int dp[1001][1001]; //인덱스 위치까지의 LCS
+
+int main() {
+	string A, B;
+	int i, j;
+
+	cin >> A >> B;
+
+	for (i = 1; i <= A.size(); i++) {
+		for (j = 1; j <= B.size(); j++) {
+			if (A[i-1] == B[j-1])
+				dp[i][j] = dp[i-1][j-1] + 1;
+			else
+				dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+		}
+	}
+
+	cout << dp[A.size()][B.size()] << endl;
+	return 0;
+}
---
+++ NameLove_BOJ15312.cpp
@@ -0,0 +1,34 @@
+﻿/*﻿
+for문을 이름의 길이만큼 반복하며 vector에 획수를 A, B 한 글자씩 차례로 저장한다.
+임시 vector을 이용해 궁합을 계산하고 원래 vector인 name을 vector temp의 내용으로 대체하는 것을 숫자가 두개만 남을 때까지 반복한다. 남은 숫자 두개를 차례로 출력한다.﻿
+*/
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	string A, B;
+	vector<int> name;
+	int stroke [26] = { 3, 2, 1, 2, 3, 3, 2, 3, 3, 2, 2, 1,
+		2, 2, 1, 2, 2, 2, 1, 2, 1, 1, 1, 2, 2, 1 };
+	int i;
+
+	cin >> A >> B;
+
+	for (i = 0; i < A.size(); i++) {
+		name.push_back(stroke[A[i] - 'A']); //몇번째 알파벳인지 찾아 획수 저장
+		name.push_back(stroke[B[i] - 'A']);
+	}
+
+	while (name.size() > 2) {
+		vector<int> temp;
+		for (int i = 0; i < name.size() - 1; i++) {
+			temp.push_back((name[i] + name[i + 1]) % 10);
+		}
+		name = temp;
+	}
+	cout << name[0] << name[1] << endl;
+	return 0;
+}
